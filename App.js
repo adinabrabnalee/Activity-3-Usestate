e6fb6af8d58c17f0e8135890606252652a3bc010
MICROLANDMIRABITE <143100396+MICROLANDMIRABITE@users.noreply.github.com>
2023-09-23 22:30:58 +0800

    1

---
+++ App.js
@@ -0,0 +1,183 @@
+import { StatusBar } from "expo-status-bar";
+import { Button, Image, StyleSheet, Text, View } from "react-native";
+import React, { useState } from "react";
+
+
+const Main = () => {
+  const [main, setmain] = useState(false);
+  const [flight, setflight] = useState(false);
+  const [counter, setcounter] = useState(false);
+  const [back, setBack] = useState(false);
+
+  const tomain = () => {
+    main ? setmain(false) : setmain(true);
+    flight ? setflight(false) : setflight(false);
+    counter ? setcounter(false) : setcounter(false);
+    back ? setBack(false) : setBack(false);
+  };
+  const toflight = () => {
+    flight ? setflight(false) : setflight(true);
+    counter ? setcounter(false) : setcounter(false);
+    main ? setmain(false) : setmain(true);
+    back ? setBack(false) : setBack(true);
+  };
+  const tocounter = () => {
+    flight ? setflight(false) : setflight(false);
+    counter ? setcounter(false) : setcounter(true);
+    main ? setmain(false) : setmain(true);
+    back ? setBack(false) : setBack(true);
+  };
+
+  return (
+    <View style={styles.heads}>
+      <View style={styles.head}>
+        <Button title="FLASHLIGHT" disabled={main} onPress={toflight} />
+        <Button title="COUNTER" disabled={main} onPress={tocounter} />
+      </View>
+      <View style={styles.body}>
+        {flight && <Flight />}
+        {counter && <Counter />}
+      </View>
+      <View style={styles.btn}>
+        {back && <Button title="BACK" onPress={tomain}/>}
+      </View>
+    </View>
+  );
+};
+
+const Flight = () => {
+  const [image, setimage] = useState(false);
+  const [text, setText] = useState("ON");
+
+  const toImage = () => {
+    image ? setimage(false) : setimage(true);
+    if (text === 'ON') {
+      setText('OFF');
+    } else {
+      setText('ON');
+    }
+  };
+
+  return (
+    <View>
+      {image &&  <Image
+            source={require("./src/on.png")}
+            style={{ width: 150, height: 150, resizeMode: "stretch" , marginVertical: 20}}
+          />
+          }
+        {!image &&  <Image
+            source={require("./src/off.png")}
+            style={{ width: 150, height: 150, resizeMode: "stretch" ,  marginVertical: 20}}
+          />
+          }
+     <Button title= {text} onPress={toImage} color="green" />
+    </View>
+  );
+};
+
+const Counter = () => {
+  const [number, setNumber] = useState(0);
+  const [main, setmain] = useState(true);
+  const [flight, setflight] = useState(false);
+  const [counter, setcounter] = useState(false);
+  const [back, setBack] = useState(true);
+
+  const tomain = () => {
+    main ? setmain(true) : setmain(false);
+    flight ? setflight(false) : setflight(false);
+    counter ? setcounter(false) : setcounter(false);
+    back ? setBack(false) : setBack(false);
+  };
+  const toflight = () => {
+    flight ? setflight(false) : setflight(true);
+    counter ? setcounter(false) : setcounter(false);
+    main ? setmain(false) : setmain(true);
+    back ? setBack(false) : setBack(true);
+  };
+  const tocounter = () => {
+    flight ? setflight(false) : setflight(false);
+    counter ? setcounter(false) : setcounter(true);
+    main ? setmain(false) : setmain(true);
+    back ? setBack(false) : setBack(true);
+  };
+
+  
+  return (
+    <View style={styles.main}>
+      <View style={styles.heads}>
+      <View style={styles.head}>
+        <Button title="FLASHLIGHT" disabled={main} onPress={toflight}  />
+        <Button title="COUNTER" disabled={main} onPress={tocounter} />
+        </View>
+        </View>
+    <Text style={styles.number}>{number}</Text>
+    <View style={styles.btnmain}>
+
+      <Button
+        title="-1" color="green"
+        onPress={() => {
+          setNumber(number - 1);
+        }}
+      />
+      <Button
+        title="+1" color="green"
+        onPress={() => {
+          setNumber(number + 1);
+        }}
+      />
+    </View>
+    <View style={styles.btn}>
+        <Button title="BACK" onPress={tomain} />
+      </View>
+    </View>
+  )
+
+};
+  
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Main />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+  },
+  head: {
+    marginTop: 40,
+    flexDirection: "row",
+    gap: 20,
+    justifyContent: "space-between",
+    paddingVertical: 20,
+  },
+  heads: {
+    flexDirection: "column",
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  number: {
+    fontSize: 100,
+    fontWeight: 'bold'
+  },
+  main: {
+    flex: 1,
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    height: 400,
+  },
+  btnmain: {
+    marginTop: 40,
+    flexDirection: "row",
+    gap: 20,
+  },
+  btn: {
+    marginTop:120,
+    width: 100,
+    paddingVertical: 20
+  },
+});
